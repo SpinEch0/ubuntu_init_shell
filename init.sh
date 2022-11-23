6d897ec7f83b8c51cb7105920a62cae52951d934
b <a@a.com>
2022-11-23 11:48:01 +0800

    add wps

--- init.sh
+++ init.sh
@@ -10,6 +10,10 @@ sudo apt install -y vim git fcitx-googlepinyin zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 sed -i 's/^ZSH_THEME.*$/ZSH_THEME=\"frisk\"/' ~/.zshrc
 
-#vscode
+# vscode
 wget -O vs.deb  https://code.visualstudio.com/sha/download\?build\=stable\&os\=linux-deb-x64
 sudo dpkg -i vs.deb
+
+# wps
+wget -O wps.deb https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2019/11664/wps-office_11.1.0.11664_amd64.deb
+sudo dpkg -i wps.deb
